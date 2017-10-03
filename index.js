41cf5cb3a838f08d177aced33c1190e72b3fe6e0
Muriel Silveira <murielkong@gmail.com>
2017-10-03 16:44:19 +0200

    Create a hello world GraphQL API

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+const root = {
+  hello: () => {
+    return 'Hello'
+  }
+}
+
+const app = express()
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}))
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(3002, () => {
+  console.log('Server running on http://127.0.0.1:3002/')
+})
